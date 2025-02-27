861002241071605544ba457ad36f700b48afdb33
Ishtiaque Akbar <akbarishtiaque@gmail.com>
2025-02-27 21:34:24 +0000

    fix padding top

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,12 +6,9 @@ import NavBar from './navbar';
 function App() {
   return (
     <div>
-      {/* Pixel-based padding-top for NavBar */}
-      <div className="pt-[32px] sm:pt-[48px] md:pt-[64px] lg:pt-[80px] xl:pt-[96px]">
-        <NavBar />
-      </div>
-      {/* Padding for CommitteeList and Socials (optional) */}
-      <div className="pt-[16px] sm:pt-[24px] md:pt-[32px] lg:pt-[40px] xl:pt-[48px]">
+      <NavBar />
+      {/* Small padding for spacing between navbar and content */}
+      <div className="pt-4">
         <CommitteeList />
         <Socials />
       </div>
